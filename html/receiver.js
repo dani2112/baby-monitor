a9523fe5f0b751c3e8d889d9fc7201c3dfd6a7cb
Daniel Klitzke <info@dk-s.de>
2016-06-01 21:15:16 +0200

    implement first example of audio playback via javascript

--- html/receiver.js
+++ html/receiver.js
@@ -1,19 +1,54 @@
-function SoundReceiver() {
-
-
+var audioContext;
+window.addEventListener('load', init, false);
+function init() {
+  try {
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    audioContext = new AudioContext();
+  }
+  catch(ex) {
+    console.log('Audio playback not supported');
+  }
 }
+
+function SoundReceiver() {
+    this.bufferedChunkCount = 3;
+    this.chunkQueue = [];
+
+    this.channelCount = 1;
+    this.sampleRate = 41000;
+    this.startTime = 0;
+}
+
+SoundReceiver.prototype.channelCount = 1;
+SoundReceiver.sampleRate = 41000;
 
 SoundReceiver.prototype.startSoundReceiving = function(url) {
     var websocket = new WebSocket('ws://192.168.178.31:8082/audio-stream');
+    websocket.binaryType = 'arraybuffer';
     websocket.onopen = function (event) {
-      console.log('abcd');
-      websocket.send("Here's some text that the server is urgently awaiting!");
+      console.log('Websocket connection opened');
     };
     websocket.onclose = function(){
-       console.log('Connection closed');
+       console.log('Websocket connection closed');
     }
+    var self = this;
     websocket.onmessage = function (event) {
-      console.log(event.data);
+        var data = event.data;
+        var dataLength = data.byteLength;
+        var dataView = new DataView(data);
+        var processedAudio = new Float32Array(dataLength);
+        for(var i = 0; i < dataLength; i++) {
+            var sample = dataView.getInt8(i);
+            processedAudio[i] = sample < 0 ? sample / 0x80 : sample / 0x7F;
+        }
+        var audioBuffer = audioContext.createBuffer(self.channelCount, dataLength, self.sampleRate);
+        audioBuffer.getChannelData(0).set(processedAudio);
+        var source = audioContext.createBufferSource();
+        source.buffer = audioBuffer;
+        source.start(self.startTime);
+        source.connect(audioContext.destination);
+        self.startTime += audioBuffer.duration;
+        console.log('Play');
     };
 }
 
