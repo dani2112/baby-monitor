59fbd0dd32b675238a7608b4a2bab06083ead3f0
Daniel Klitzke <info@dk-s.de>
2016-06-04 22:08:53 +0200

    implement scheduler function in javascript

--- html/receiver.js
+++ html/receiver.js
@@ -11,15 +11,26 @@ function init() {
 }
 
 function SoundReceiver() {
-    this.bufferedChunkCount = 3;
+    this.bufferedMinCount = 3;
     this.chunkQueue = [];
 
     this.channelCount = 1;
     this.sampleRate = 44100;
-    this.startTime = 0;
 }
 
 SoundReceiver.prototype.startSoundReceiving = function(url) {
+    /* Keep this reference in nested functions */
+    var self = this;
+    /* Start sound scheduler */
+    setTimeout(function schedule() {
+        var element = self.chunkQueue.shift();
+        if(element != undefined) {
+            console.log(element.timestamp);
+        }
+        setTimeout(schedule, 100);
+    }, 0);
+
+    /* Open websocket connection */
     var websocket = new WebSocket('ws://192.168.178.31:8082/audio-stream');
     websocket.binaryType = 'arraybuffer';
     websocket.onopen = function (event) {
@@ -28,23 +39,17 @@ SoundReceiver.prototype.startSoundReceiving = function(url) {
     websocket.onclose = function(){
        console.log('Websocket connection closed');
     }
-    var self = this;
     websocket.onmessage = function (event) {
         var data = event.data;
         var dataLength = data.byteLength;
         var processedAudio = self.convertData(data);
-        var audioBuffer = audioContext.createBuffer(self.channelCount, dataLength, self.sampleRate);
-        audioBuffer.getChannelData(0).set(processedAudio);
-        var source = audioContext.createBufferSource();
-        source.buffer = audioBuffer;
-        source.start(self.startTime);
-        self.startTime = audioContext.currentTime + audioBuffer.duration;
-        source.connect(audioContext.destination);
+        var currentTimestamp = audioContext.currentTime;
+        self.chunkQueue.push({data: processedAudio, timestamp: currentTimestamp});
     };
 }
 
 SoundReceiver.prototype.convertData = function(data) {
-var dataLength = data.byteLength;
+        var dataLength = data.byteLength;
         var dataView = new DataView(data);
         var processedAudio = new Float32Array(dataLength);
         for(var i = 0; i < dataLength; i++) {
@@ -52,6 +57,17 @@ var dataLength = data.byteLength;
             processedAudio[i] = sample < 0 ? sample / 0x80 : sample / 0x7F;
         }
         return processedAudio;
+}
+
+SoundReceiver.prototype.playSound = function(data, time) {
+        var self = this;
+        var dataLength = data.byteLength;
+        var audioBuffer = audioContext.createBuffer(self.channelCount, dataLength, self.sampleRate);
+        audioBuffer.getChannelData(0).set(processedAudio);
+        var source = audioContext.createBufferSource();
+        source.buffer = audioBuffer;
+        source.start(time);
+        source.connect(audioContext.destination);
 }
 
 var receiver = new SoundReceiver();
