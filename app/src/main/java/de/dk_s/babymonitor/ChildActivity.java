17acd6bb5878e8241f42589bb4b22b4b26c28ae6
Daniel Klitzke <info@dk-s.de>
2016-06-12 17:00:01 +0200

    provide up navigation functionality

--- app/src/main/java/de/dk_s/babymonitor/ChildActivity.java
+++ app/src/main/java/de/dk_s/babymonitor/ChildActivity.java
@@ -9,5 +9,6 @@ public class ChildActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
