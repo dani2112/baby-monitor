a366552a1f907bb584f946f95513875083b338d2
Daniel Klitzke <info@dk-s.de>
2016-06-13 15:49:31 +0200

    create fragment dummy

--- app/src/main/java/de/dk_s/babymonitor/ChildActivity.java
+++ app/src/main/java/de/dk_s/babymonitor/ChildActivity.java
@@ -1,14 +1,20 @@
 package de.dk_s.babymonitor;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ChildActivity extends AppCompatActivity {
+public class ChildActivity extends AppCompatActivity implements SoundAnimationFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
