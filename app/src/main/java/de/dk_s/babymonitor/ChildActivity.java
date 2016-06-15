be79c55930a3c6c291f1f127f8c475b50fc37b58
Daniel Klitzke <info@dk-s.de>
2016-06-15 15:26:00 +0200

    first try to set button if service is already running

--- app/src/main/java/de/dk_s/babymonitor/ChildActivity.java
+++ app/src/main/java/de/dk_s/babymonitor/ChildActivity.java
@@ -1,7 +1,11 @@
 package de.dk_s.babymonitor;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.Uri;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,24 +21,74 @@ public class ChildActivity extends AppCompatActivity implements SoundAnimationFr
 
     private Intent lastServiceIntent;
 
+    private MonitoringService monitoringService = null;
+
+    private boolean isBound = true;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            monitoringService = ((MonitoringService.MonitoringServiceBinder)service).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            monitoringService = null;
+        }
+    };
+
+    private void doBindService() {
+        lastServiceIntent = new Intent(this, MonitoringService.class);
+        bindService(lastServiceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        isBound = true;
+    }
+
+    private void doUnbindService() {
+        if (isBound) {
+            // Detach our existing connection.
+            unbindService(serviceConnection);
+            isBound = true;
+        }
+    }
+
+    private CompoundButton.OnCheckedChangeListener checkedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(isChecked) {
+                startService(lastServiceIntent);
+            } else {
+                stopService(lastServiceIntent);
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        doBindService();
+
         ToggleButton onOffToggleButton = (ToggleButton)findViewById(R.id.onOffToggleButton);
-        onOffToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    lastServiceIntent = new Intent(ChildActivity.this, MonitoringService.class);
-                    startService(lastServiceIntent);
-                } else {
-                    stopService(lastServiceIntent);
-                }
+        onOffToggleButton.setOnCheckedChangeListener(checkedChangeListener);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        doUnbindService();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        if(monitoringService != null) {
+            ToggleButton onOffToggleButton = (ToggleButton)findViewById(R.id.onOffToggleButton);
+            if (monitoringService.isStarted()) {
+                onOffToggleButton.setOnCheckedChangeListener(null);
+                onOffToggleButton.setChecked(true);
+                onOffToggleButton.setOnCheckedChangeListener(checkedChangeListener);
+            } else {
+                onOffToggleButton.setChecked(false);
             }
-        });
+        }
     }
 
     @Override
--- app/src/main/java/de/dk_s/babymonitor/monitoring/MonitoringService.java
+++ app/src/main/java/de/dk_s/babymonitor/monitoring/MonitoringService.java
@@ -2,6 +2,7 @@ package de.dk_s.babymonitor.monitoring;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.provider.MediaStore;
 import android.widget.Toast;
@@ -41,30 +42,48 @@ public class MonitoringService extends Service {
         }
     }
 
+    public class MonitoringServiceBinder extends Binder {
+        public MonitoringService getService() {
+            return MonitoringService.this;
+        }
+    }
+
     private static final String TAG = "MonitoringService";
 
+    private final IBinder binder = new MonitoringServiceBinder();
+
+
+    private boolean isStarted = false;
+
     public MonitoringService() {
+    }
+
+    public boolean isStarted() {
+        return isStarted;
     }
 
     @Override
     public void onCreate () {
         Toast.makeText(this, "service created", Toast.LENGTH_SHORT).show();
+        isStarted = false;
     }
 
     public void onDestroy() {
         Toast.makeText(this, "service destroyed", Toast.LENGTH_SHORT).show();
+        isStarted = false;
     }
 
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+        isStarted = true;
         return START_STICKY;    // restart service if it is killed by system and resources become available
     }
+
 }
