f5cd887f92cda8e6ed9cdb3aec2f410a51e88b56
Daniel Klitzke <info@dk-s.de>
2016-07-09 16:21:18 +0200

    add gui elements to parent activity

--- app/src/main/java/de/dk_s/babymonitor/ParentActivity.java
+++ app/src/main/java/de/dk_s/babymonitor/ParentActivity.java
@@ -1,14 +1,29 @@
 package de.dk_s.babymonitor;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class ParentActivity extends AppCompatActivity {
+import de.dk_s.babymonitor.gui.SoundAnimationFragment;
+import de.dk_s.babymonitor.gui.eventlist.MonitorEventFragment;
+import de.dk_s.babymonitor.monitoring.BabyVoiceMonitor;
+
+public class ParentActivity extends AppCompatActivity implements SoundAnimationFragment.OnFragmentInteractionListener, MonitorEventFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(BabyVoiceMonitor.AudioEvent item) {
+
     }
 }
